ecbba0d34b449159cefd1c329696ac477b8168f9
hydrochin <hidro296@gmail.com>
2024-01-14 23:52:17 +0700

    my last miserable days end

---
+++ CodeGeneration/day3 - Copy/incompleted/main.c
@@ -0,0 +1,45 @@
+/*
+ * @copyright (c) 2008, Hedspi, Hanoi University of Technology
+ * @author Huu-Duc Nguyen
+ * @version 1.0
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "reader.h"
+#include "parser.h"
+#include "codegen.h"
+
+/******************************************************************/
+
+int main(int argc, char* argv[]) {
+	///*
+	if (argc <= 1) {
+		printf("kplc: no input file.\n");
+		return -1;
+	}	
+	initCodeBuffer();
+	if (compile(argv[1]) == IO_ERROR) {
+		printf("Can\'t read input file!\n");
+		return -1;
+	}	
+
+	printf("\n\n");
+	printCodeBuffer();
+	
+	//cleanCodeBuffer();
+	//*/
+	//Debug trên Visual Studio 2022
+	/*
+	initCodeBuffer();
+	if (compile("../tests/example2.kpl") == IO_ERROR) {
+		printf("Can\'t read input file!\n");
+		return -1;
+	}
+	printf("\n\n");
+	printCodeBuffer();
+	*/
+	return 0;
+}
